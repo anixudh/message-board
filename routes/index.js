af55efaa717c550d0fb9c2513cc9f514223569dc
anixudh <anirudh.varma196@gmail.com>
2022-01-28 21:33:29 +0530

    final

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "ani",
+    added: new Date(),
+  },
+  {
+    text: "Good day!",
+    user: "rudh",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.authorName,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
